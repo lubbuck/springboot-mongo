d9352f07fc497316100c0d8d5fdf1e097ba8c40e
Antonio <antonioluisp97@gmail.com>
2024-01-15 17:55:14 -0300

    projeto criado

---
+++ src/test/java/com/lubbuck/springbootmongo/SpringbootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubbuck.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
