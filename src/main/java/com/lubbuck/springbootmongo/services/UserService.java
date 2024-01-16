c1e87dd0874bc1c4db7295c1b8b406be33b969af
Antonio <antonioluisp97@gmail.com>
2024-01-16 20:08:26 -0300

    feat insercao de user

--- src/main/java/com/lubbuck/springbootmongo/services/UserService.java
+++ src/main/java/com/lubbuck/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lubbuck.springbootmongo.domain.User;
+import com.lubbuck.springbootmongo.dto.UserDTO;
 import com.lubbuck.springbootmongo.repositories.UserRepository;
 import com.lubbuck.springbootmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado. Id: " + id));
+	}
+
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
