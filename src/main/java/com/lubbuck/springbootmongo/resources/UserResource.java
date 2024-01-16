c0a5aecb3da638fe19408a0bd79b172e4fd7f7c1
Antonio <antonioluisp97@gmail.com>
2024-01-16 19:07:15 -0300

    feat user entity mapeada, user service e repository conectando com o mongo

--- src/main/java/com/lubbuck/springbootmongo/resources/UserResource.java
+++ src/main/java/com/lubbuck/springbootmongo/resources/UserResource.java
@@ -1,27 +1,26 @@
 package com.lubbuck.springbootmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lubbuck.springbootmongo.domain.User;
+import com.lubbuck.springbootmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "maria", "maria@gmail.com");
-		User alex = new User("2", "alex", "alex@gmail.com");
-		List<User> users = new ArrayList<>();
-		users.addAll(Arrays.asList(maria, alex));
-
+		List<User> users = userService.findAll();
 		return ResponseEntity.ok().body(users);
 	}
 }
