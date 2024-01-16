ba85775e81fb79fba2115cabf7261291464a1478
Antonio <antonioluisp97@gmail.com>
2024-01-16 19:49:39 -0300

    feat metodo findByid do user com tratamento de excecao

--- src/main/java/com/lubbuck/springbootmongo/services/UserService.java
+++ src/main/java/com/lubbuck/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lubbuck.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lubbuck.springbootmongo.domain.User;
 import com.lubbuck.springbootmongo.repositories.UserRepository;
+import com.lubbuck.springbootmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado. Id: " + id));
 	}
 }
