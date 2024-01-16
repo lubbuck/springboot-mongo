79a3f4fec48c69f1fae43bbd861fb0e71d3f1c4b
Antonio <antonioluisp97@gmail.com>
2024-01-16 19:22:39 -0300

    feat usando o padrão dto para retornar os ususarios

--- src/main/java/com/lubbuck/springbootmongo/resources/UserResource.java
+++ src/main/java/com/lubbuck/springbootmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lubbuck.springbootmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lubbuck.springbootmongo.domain.User;
+import com.lubbuck.springbootmongo.dto.UserDTO;
 import com.lubbuck.springbootmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = userService.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> usersDTO = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(usersDTO);
 	}
 }
