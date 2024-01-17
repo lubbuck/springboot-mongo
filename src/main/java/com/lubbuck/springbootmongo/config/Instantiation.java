ef038fcf51c97826c94f3b51f13d57d3e2b4b8cf
Antonio <antonioluisp97@gmail.com>
2024-01-17 15:54:07 -0300

    feat comentarios para os posts

--- src/main/java/com/lubbuck/springbootmongo/config/Instantiation.java
+++ src/main/java/com/lubbuck/springbootmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lubbuck.springbootmongo.domain.Post;
 import com.lubbuck.springbootmongo.domain.User;
 import com.lubbuck.springbootmongo.dto.AuthorDTO;
+import com.lubbuck.springbootmongo.dto.CommentDTO;
 import com.lubbuck.springbootmongo.repositories.PostRepository;
 import com.lubbuck.springbootmongo.repositories.UserRepository;
 
@@ -42,10 +43,17 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hj", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		
+
 		userRepository.save(maria);
 	}
 
--- src/main/java/com/lubbuck/springbootmongo/domain/Post.java
+++ src/main/java/com/lubbuck/springbootmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.lubbuck.springbootmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lubbuck.springbootmongo.dto.AuthorDTO;
+import com.lubbuck.springbootmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -72,6 +77,10 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,5 +97,4 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
